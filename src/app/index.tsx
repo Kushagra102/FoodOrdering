7b1466bb1b1f5f7d68886569fe74a0545ad47d6d
Kushagra102 <kushagra.garg77@gmail.com>
2024-02-20 19:33:15 +0530

    Admin side create product screen

---
+++ src/app/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Button from '../components/Button';
+import { Link } from 'expo-router';
+
+const index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+      <Link href={'/(admin)'} asChild>
+        <Button text="Admin" />
+      </Link>
+      <Link href={'/(user)'} asChild>
+        <Button text="User" />
+      </Link>
+    </View>
+  );
+};
+
+export default index;
