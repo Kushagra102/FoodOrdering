b874561982107f20ee60b59c0e9541330ac1b2b9
Kushagra102 <kushagra.garg77@gmail.com>
2024-02-21 02:06:17 +0530

    Auth Completed, Routes Protected, Supabase Init

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://spnrworwmvuclztfhamn.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbnJ3b3J3bXZ1Y2x6dGZoYW1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0NTY4MjUsImV4cCI6MjAyNDAzMjgyNX0.h5t6cwYefcz-gQFOmoc_FyvRC_4v__r5DJpfWqcw1sY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
