aa989130579157d7ae636171d2987d5abed27253
Kushagra102 <kushagra.garg77@gmail.com>
2024-02-25 21:45:34 +0530

    Stripe with supabase edge functions

---
+++ supabase/functions/payment-sheet/index.ts
@@ -0,0 +1,41 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { stripe } from "../_utils/stripe.ts";
+
+console.log("Hello from Functions!");
+
+Deno.serve(async (req) => {
+  try {
+    const { amount } = await req.json();
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+    });
+
+    const res = {
+      paymentIntent: paymentIntent.client_secret,
+      publishableKey: Deno.env.get("EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY"),
+    };
+
+    return new Response(
+      JSON.stringify(res),
+      { headers: { "Content-Type": "application/json" } },
+    );
+  } catch (error) {
+    console.log("Error", error);
+    return new Response(
+      JSON.stringify(error),
+      { status: 400, headers: { "Content-Type": "application/json" } },
+    );
+  }
+});
+
+// curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"amount":1150}'
+
+
+// curl -i --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet'   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'   --header 'Content-Type: application/json'   --data '{"amount":1099}'
